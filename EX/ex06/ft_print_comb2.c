8e456fc44d1381ee058082103fa06eb94f527662
BennieBickles <marie.tortrotau@gmail.com>
2023-09-07 09:16:39 -0400

    <3

--- EX/ex06/ft_print_comb2.c
+++ EX/ex06/ft_print_comb2.c
@@ -14,16 +14,19 @@
 
 void	ft_print_number(int n)
 {
-	int		n1;
+	int		n1; //On cree deux variables dans lesquelles on va stocker des valeurs
 	int		n2;
-
-	n1 = n / 10 + 48;
-	n2 = n % 10 + 48;
-	write(1, &n1, 1);
+//Pour afficher un nombre, on part du principe qu'il faut deux chiffres (37 = 3 et 7)
+//Il n'y a pas de nombres dans la table ascii d'ou cette necessite 
+	n1 = n / 10 + 48; //Pour decomposer notre nombre, on commence par diviser le nombre par 10 pour obtenir le premier chiffre de notre nombre. Ici en locurence, avec 37 --> 37/10 = 3x10 + 7 (on ne garde que les entiers). Le "48" permet a la machine de comprendre qu'il s'agit d'une valeur chiffre. 48 dans la table ascii c'est 0. On aurait aussi pu ecrire la ligne comme ca : 
+			  //n1 = n / 10 + '0'; 
+	n2 = n % 10 + 48; //Meme logique sauf que ce n'est pas "/" de la division mais % (on appelle ca modulo). Le modulo c'est le reste de l'opperation de la division. Dans mon exemple : 37 / 10 = 3 mais il reste le 7. Le modulo correspond au reste d'une division. DONC 37 % 10 = 7
+			  //On stocke donc dans n2 le deuxieme chiffre qu'on veut afficher. Ici le 48 ou '0' permet toujours a la machine d'attribuer la valeur chiffre au resultat de l'operation. 
+	write(1, &n1, 1); //Apres, tu connais, on affiche les deux valeurs qu'on a recuperees precedement
 	write(1, &n2, 1);
 }
 
-void	ft_print_comb2(void)
+void	ft_print_comb2(void) //Meme logique que le precedent mais en plus simple dans l'absolu 
 {
 	int	a;
 	int	b;
@@ -43,4 +46,12 @@ void	ft_print_comb2(void)
 		}
 		++a;
 	}
+}
+
+//Pour tester 
+//
+int	main(void)
+{
+	ft_print_comb2(void);
+	return (0);
 }
